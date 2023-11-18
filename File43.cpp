76fcd22d5b82aa2c8a91aa609d9e782e9bda3d55
MetaMsa <mserhataslan@hotmail.com>
2023-11-19 01:04:44 +0300

    fpdfsdkf

---
+++ File43.cpp
@@ -0,0 +1,11 @@
+#include <iostream.h>
+#include <conio.h>
+main()
+{
+ for(int i=0;i<128;i++)
+ {
+	 char a=i;
+	 cout<<i<<"="<<a<<endl;
+ }
+ getch();
+}
---
+++ File44.cpp
@@ -0,0 +1,13 @@
+#include <iostream.h>
+#include <conio.h>
+
+main()
+{
+ char a;
+ cin>>a;
+ if(a==27)
+ {
+    cout<<"fsdfldsþf";
+ }
+ getch();
+}
